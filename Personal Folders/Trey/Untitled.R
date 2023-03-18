c3f0e6cfdb7c92720b1d6d8d6691df3be9d8fb73
Trey L <90742156+UptoCode99@users.noreply.github.com>
2023-03-18 09:54:29 -0600

    Addings the cool data

--- Personal Folders/Trey/Untitled.R
+++ Personal Folders/Trey/Untitled.R
@@ -1,1 +1,5 @@
-print('Hello World')
+library(tidyverse)
+
+Journals <-csv_read("https://raw.githubusercontent.com/BYUIDSconsulting/woodruff_stories/master/personal_folders/treylusk/WW_by_date_with_may.csv")
+
+View("Journals")
